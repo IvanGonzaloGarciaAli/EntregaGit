496228e729aee15f4a124b455b63a8b47a68601c
Ivan <ivan.garcia205@educa.madrid.org>
2023-11-16 09:06:11 +0100

    Primer commit

---
+++ PROGRAMACION/Ejercicio4.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+import java.util.Arrays;
+
+public class Ejercicio4 {
+
+
+
+    public static void main(String[] args) {
+
+       int num;
+
+       int[] primitiva = new int[6];
+
+       Random aleatorio = new Random();
+
+        for (int i = 0; i < 6; i++) {
+
+            do {
+
+                num=aleatorio.nextInt(49)+1;
+
+                
+
+            } while (Comprobador(primitiva, num));
+
+            
+
+            primitiva[i]=num;
+
+        }
+
+        Arrays.sort(primitiva);
+
+        System.out.println("El numero ganador es: "+Arrays.toString(primitiva));
+
+        
+
+    }
+
+    public static boolean Comprobador(int [] array,int num){
+
+        boolean esta = false;
+
+        for (int i = 0; i < array.length && !esta; i++) {
+
+            if (array[i]==num)
+
+                esta = true;
+
+        }
+
+        
+
+        return esta;
+
+    
+
+    
+
+
+
+}
+
+}
